8c9970101ef84b50e05020e858253e05e6363fd2
SantinSimone <Santin716@gmail.com>
2025-02-06 18:42:43 +0100

    classi progettate

--- src/main/java/Film.java
+++ src/main/java/Film.java
@@ -1,2 +1,79 @@
+import java.util.List;
+import org.bson.Document;
+import java.util.ArrayList;
 public class Film {
+    private String ImdbId;
+    private String _id;
+    private String name;
+    private String poster_url;
+    private String year;
+    private String certificate;
+    private String runtime;
+    private List<String> genre;
+    private String ratingValue;
+    private String summary_text;
+    private String ratingCount;
+    private Person director;
+    private List<Person> cast;
+
+    public Film(String imdbId, String id, String name, String posterUrl, String year, String certificate,
+                 String runtime, List<String> genre, String ratingValue, String summaryText,
+                 String ratingCount, Person director, List<Person> cast) {
+        this.ImdbId = imdbId;
+        this._id = id;
+        this.name = name;
+        this.poster_url = posterUrl;
+        this.year = year;
+        this.certificate = certificate;
+        this.runtime = runtime;
+        this.genre = genre;
+        this.ratingValue = ratingValue;
+        this.summary_text = summaryText;
+        this.ratingCount = ratingCount;
+        this.director = director;
+        this.cast = cast;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "ImdbId='" + ImdbId + '\'' +
+                ", _id='" + _id + '\'' +
+                ", name='" + name + '\'' +
+                ", poster_url='" + poster_url + '\'' +
+                ", year='" + year + '\'' +
+                ", certificate='" + certificate + '\'' +
+                ", runtime='" + runtime + '\'' +
+                ", genre=" + genre +
+                ", ratingValue='" + ratingValue + '\'' +
+                ", summary_text='" + summary_text + '\'' +
+                ", ratingCount='" + ratingCount + '\'' +
+                ", director=" + director +
+                ", cast=" + cast +
+                '}';
+    }
+    public Document toDocument() {
+        Document document = new Document("ImdbId", this.ImdbId)
+                .append("_id", this._id)
+                .append("name", this.name)
+                .append("poster_url", this.poster_url)
+                .append("year", this.year)
+                .append("certificate", this.certificate)
+                .append("runtime", this.runtime)
+                .append("genre", this.genre)
+                .append("ratingValue", this.ratingValue)
+                .append("summary_text", this.summary_text)
+                .append("ratingCount", this.ratingCount);
+
+        Document directorDocument = this.director != null ? this.director.toDocument() : null;
+        document.append("director", directorDocument);
+
+        List<Document> castDocuments = new ArrayList<>();
+        for (Person person : this.cast) {
+            castDocuments.add(person.toDocument());
+        }
+        document.append("cast", castDocuments);
+
+        return document;
+    }
 }
--- src/main/java/MongoDBConnection.java
+++ src/main/java/MongoDBConnection.java
@@ -1,2 +1,92 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class MongoDBConnection {
+    public static void main(String[] args) {
+        String connectionString = "mongodb://localhost:27017";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            MongoDatabase database = mongoClient.getDatabase("mongoDB");
+            System.out.println("Connesso al database: " + database.getName());
+
+            MongoCollection<Document> collection = database.getCollection("myCollection");
+            List<Document> movies = getData();
+            collection.insertMany(movies);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Document> getData() {
+        List<Document> movies = new ArrayList<>();
+
+        try (FileReader reader = new FileReader("Movies.json")) {
+
+            JSONTokener tokener = new JSONTokener(reader);
+            JSONArray jsonArray = new JSONArray(tokener);
+
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String imdbId = jsonObject.optString("ImdbId", "");
+                String id = jsonObject.optString("_id", "");
+                String name = jsonObject.optString("name", "");
+                String posterUrl = jsonObject.optString("poster_url", "");
+                String year = jsonObject.optString("year", "");
+                String certificate = jsonObject.optString("certificate", "");
+                String runtime = jsonObject.optString("runtime", "");
+                String ratingValue = jsonObject.optString("ratingValue", "");
+                String summaryText = jsonObject.optString("summary_text", "");
+                String ratingCount = jsonObject.optString("ratingCount", "");
+
+                JSONArray genreArray = jsonObject.optJSONArray("genre");
+                List<String> genre = new ArrayList<>();
+                if (genreArray != null) {
+                    for (int j = 0; j < genreArray.length(); j++) {
+                        genre.add(genreArray.optString(j, ""));
+                    }
+                }
+
+                JSONObject directorObject = jsonObject.optJSONObject("director");
+                Person director = null;
+                if (directorObject != null) {
+                    String directorName = directorObject.optString("name", "");
+                    String directorNameId = directorObject.optString("name_id", "");
+                    director = new Person(directorName, directorNameId);
+                }
+
+                JSONArray castArray = jsonObject.getJSONArray("cast");
+                List<Person> cast = new ArrayList<>();
+                for (int j = 0; j < castArray.length(); j++) {
+                    JSONObject personObject = castArray.getJSONObject(j);
+                    String personName = personObject.optString("name", "");
+                    String personNameId = personObject.optString("name_id", "");
+                    Person person = new Person(personName, personNameId);
+                    cast.add(person);
+                }
+
+                Film movie = new Film(imdbId, id, name, posterUrl, year, certificate, runtime,
+                        genre, ratingValue, summaryText, ratingCount, director, cast);
+                movies.add(movie.toDocument());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return movies;
+    }
 }
